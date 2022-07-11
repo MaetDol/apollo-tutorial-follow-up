e67750bfacd93256d5635ada6ef80db8eb5632dd
MaetDol <rndka9@gmail.com>
2022-07-12 01:19:55 +0900

    Lift-off III 클라이언트 작업 완료

---
+++ odyssey-lift-off-part3/client/src/pages/track.js
@@ -0,0 +1,43 @@
+import { gql, useQuery } from '@apollo/client';
+import TrackDetail from '../components/track-detail';
+
+const { Layout, QueryResult } = require('../components');
+
+export const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
+
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
