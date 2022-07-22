3d61b12d19de3dba5621272552d30f9531fbfe58
MaetDol <rndka9@gmail.com>
2022-07-23 01:03:31 +0900

    Lift-off V 초기화 커밋

---
+++ odyssey-lift-off-part5-client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  cache: new InMemoryCache(),
+  name: "web",
+  version: "1.0",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
---
+++ odyssey-lift-off-part5-server/src/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
+
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
+
+  const { url, port } = await server.listen();
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer(typeDefs, resolvers);
